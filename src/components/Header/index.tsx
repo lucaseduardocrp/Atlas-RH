211b567adb33534309f6b8f6994a77feb70ae488
Lucas Carvalho <lucaseduardocrp@gmail.com>
2023-06-18 12:12:38 -0300

    create header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,7 @@
+'use client'
+
+import { useState } from "react";
+
 import Image from "next/image";
 import Link from "next/link";
 
@@ -6,27 +10,42 @@ import Navlink from "../Navlink";
 import styles from './styles.module.scss';
 
 export default function Header() {
+  const [active, setActive] = useState(false)
+
+  const handleActive = () => {
+    setActive(!active)
+  }
+
   return(
-    <header>
+    <header className={styles.header}>
       <nav className={styles.navbar}>
         <Link href={'/'}>
-          <Image src='/Logo.png' width={181} height={60} alt="Logo Atlas Consultoria de RH" />
+          <Image src='/Logo.png' width={161} height={45} alt="Logo Atlas Consultoria de RH" />
         </Link>
+
+        <div className={styles.menu}>
+          <div
+            onClick={handleActive}
+            className={!active ? styles.mobileMenu : styles.mobileMenuActive}
+          />
+        </div>
         
-        <ul className={styles.navlist}>
-          <li>
-            <Navlink className={styles.navlink} href={'/'}>Home</Navlink>
-          </li>
-          <li>
-            <Navlink className={styles.navlink} href={'/About'}>About Us</Navlink>
-          </li>
-          <li>
-            <Navlink className={styles.navlink} href={''}>Services</Navlink>
-          </li>
-          <li>
-            <Navlink className={styles.navlink} href={''}>Contact</Navlink>
-          </li>
-        </ul>
+        <div className={active ? styles.mobileMenuOpen : styles.mobileMenuClose}>
+          <ul className={styles.navlist}>
+            <li>
+              <Navlink href={'/'}>Home</Navlink>
+            </li>
+            <li>
+              <Navlink href={'/About'}>About Us</Navlink>
+            </li>
+            <li>
+              <Navlink href={'/ServicesPage'}>Services</Navlink>
+            </li>
+            <li>
+              <Navlink href={'/Contact'}>Contact</Navlink>
+            </li>
+          </ul>
+        </div>
       </nav>
     </header>
   )
--- src/components/Navlink/index.tsx
+++ src/components/Navlink/index.tsx
@@ -4,18 +4,19 @@ import Link, { LinkProps } from "next/link";
 import { usePathname } from "next/navigation";
 import { ComponentPropsWithoutRef, ReactNode } from "react";
 
+import styles from './styles.module.scss';
+
 type CustomLinkProps = {
   children: ReactNode
-  className: string
 } & LinkProps & ComponentPropsWithoutRef<'a'>
 
-export default function Navlink({ children, href, className, ...rest}: CustomLinkProps) {
+export default function Navlink({ children, href, ...rest}: CustomLinkProps) {
   const pathname = usePathname()
 
   const isCurrentPath = pathname === href || pathname === rest.as || pathname?.startsWith(String(rest.as));
 
   return(
-    <Link {...rest} href={href} className={isCurrentPath ? className : ''}>
+    <Link {...rest} href={href} className={isCurrentPath ? styles.navlink : ''}>
       {children}
     </Link>
   )
